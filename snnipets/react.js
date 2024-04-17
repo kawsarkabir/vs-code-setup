4025337bf0ee363ffead4bb8f02288a9cb85a4d0
kawsarkabir <devkawsarkabir@gmail.com>
2024-04-17 19:26:48 +0600

    react snnipets added

---
+++ snnipets/react.js
@@ -0,0 +1,15 @@
+1. // React functional component using command 'rfc'
+    "React component": {
+		"prefix": "rfc",
+		"body": [
+			"export default function $TM_FILENAME_BASE(){",
+			"    return (",
+			"         <>",
+			"            $2",
+			"         </>",
+			"    );",
+			"}"
+		],
+		"description": "React functional component"
+}
+
